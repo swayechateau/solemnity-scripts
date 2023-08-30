f53b066c0d5730f43ab719f19622a630a9edd645
Swaye Chateau <swaye@kitechsoftware.com>
2023-08-30 03:48:24 +0100

    backup linux script

---
+++ linux/setup.sh
@@ -0,0 +1,43 @@
+# get linux kernel version
+LINUX_KERNEL=$(uname -r)
+
+# get distro and version
+if [ -f /etc/os-release ]; then
+    # freedesktop.org and systemd
+    . /etc/os-release
+    OS=$NAME
+    VER=$VERSION_ID
+elif type lsb_release >/dev/null 2>&1; then
+    # linuxbase.org
+    OS=$(lsb_release -si)
+    VER=$(lsb_release -sr)
+elif [ -f /etc/lsb-release ]; then
+    # For some versions of Debian/Ubuntu without lsb_release command
+    . /etc/lsb-release
+    OS=$DISTRIB_ID
+    VER=$DISTRIB_RELEASE
+elif [ -f /etc/debian_version ]; then
+    # Older Debian/Ubuntu/etc.
+    OS=Debian
+    VER=$(cat /etc/debian_version)
+elif [ -f /etc/SuSe-release ]; then
+    # Older SuSE/etc.
+    ...
+elif [ -f /etc/redhat-release ]; then
+    # Older Red Hat, CentOS, etc.
+    ...
+else
+    # Fall back to uname, e.g. "Linux <version>", also works for BSD, etc.
+    OS=$(uname -s)
+    VER=$(uname -r)
+fi
+
+
+echo $OS
+echo $VER
+SETUP_DISTRO="./distros/$OS-$VER" | awk '{print tolower($0)}'
+echo $SETUP_DISTRO
+# get packages
+# yum
+# apt
+# dnf
